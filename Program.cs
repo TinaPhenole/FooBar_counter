111c8bba5d945075600d3aae35d1931a90ab92bd
TinaPhenole <91chrjoh@skola.boras.se>
2022-07-03 17:01:11 +0200

    Finished code example

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FooBar_counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            while (i < 30)
+            {
+                i++;
+
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("Foo Bar");
+                }
+                else if(i % 3 == 0)
+                {
+                    Console.WriteLine("Foo");
+                }
+
+                else if(i % 5 == 0)
+                {
+                    Console.WriteLine("Bar");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }  
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Counter 1 - 30 with Foo Bar
+
+## Assignment
+
+Write code that prints out numbers 1 - 30 in the console.
+
+1. If the number is dividable by 3, insteed of number, write out 'Foo'.
+2. If the number is dividable by 5, insteed of number, write out 'Bar'.
+3. If the number is dividable by 3 AND 5, insteed of number, write out 'Foo Bar'.
